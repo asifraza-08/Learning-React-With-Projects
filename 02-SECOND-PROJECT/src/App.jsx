4c9cd04104d9e6dbd419835e0e3781667a7b2f57
asifraza-08 <YOUR_GITHUB_EMAIL>
2026-03-10 10:05:59 +0530

    daily progress

--- 02-SECOND-PROJECT/src/App.jsx
+++ 02-SECOND-PROJECT/src/App.jsx
@@ -1,16 +1,34 @@
 /* eslint-disable no-unused-vars */
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  // let count = 10;
 
+  let [counter, setCounter] = useState(10);
+
+  function addValue() {
+    setCounter(counter + 1);
+  }
+
+  function removeValue() {
+    setCounter(counter > 0 ? counter - 1 : 0);
+  }
+
+  // counter is updating the value but that is not visible on our UI, so here react controls that using USE STATE HOOK
+  // ASSIGNMENT QUESTION IS THAT THE VALUE CAN'T GO IN NEGATIVE WHEN WE ARE DECREASING THE VALUE
 
   return (
     <>
-      
-    
+      <h1>Counter:{counter}</h1>
+      <h2>Counter Value:{counter}</h2>
+      <div>
+        <button onClick={addValue}>Increase Value:{counter}</button>
+        <br /> <br />
+        <button onClick={removeValue}>Decrease Value:{counter}</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
